2eb88b26f764e77e7181a49b2fc37b3e7c07c092
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-11 23:33:30 +0300

    homework 8

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector('#btn');
+
+const body = document.querySelector('body');
+
+const value = document.querySelector('span.color');
+
+const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']; 
+
+button.addEventListener('click', getValue);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
