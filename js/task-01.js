de3a15c1f2a10e1272c03956994911ed0f885ecf
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-12 23:46:44 +0300

    Сomit

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,33 @@
-const ctgr = document.querySelector('#categories');
-console.log ('Number of categories',ctgr.children.length);
-
-const firstChild = ctgr.firstElementChild
-
-console.log ('Category:',firstChild.querySelector('h2').textContent);
-console.log ('Elements:',firstChild.querySelectorAll('li').length);
-
-const secondChild = firstChild.nextElementSibling
-
-console.log ('Category:',secondChild.querySelector('h2').textContent);
-console.log ('Elements:',secondChild.querySelectorAll('li').length);
+const NumberOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", NumberOfCategories.length);
+NumberOfCategories.forEach((el) => {
+  const categoriesTitle = el.querySelector("h2").innerHTML;
+  console.log("Category:", categoriesTitle);
+  const itemsLength = el.querySelectorAll("li").length;
+  console.log(" Elements:", itemsLength);
+});
 
 
-const thirdChild = secondChild.nextElementSibling
 
-console.log ('Category:',thirdChild.querySelector('h2').textContent);
-console.log ('Elements:',thirdChild.querySelectorAll('li').length);
+
+
+
+
+
+
+
+// const firstChild = ctgr.firstElementChild
+
+// console.log ('Category:',firstChild.querySelector('h2').textContent);
+// console.log ('Elements:',firstChild.querySelectorAll('li').length);
+
+// const secondChild = firstChild.nextElementSibling
+
+// console.log ('Category:',secondChild.querySelector('h2').textContent);
+// console.log ('Elements:',secondChild.querySelectorAll('li').length);
+
+
+// const thirdChild = secondChild.nextElementSibling
+
+// console.log ('Category:',thirdChild.querySelector('h2').textContent);
+// console.log ('Elements:',thirdChild.querySelectorAll('li').length);
