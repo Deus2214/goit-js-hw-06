512d354bf328f7fb6927c14e222f9ce6b36e9e87
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-12 16:14:08 +0300

    Homework done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form')
+const data = {
+    email:"",
+    password: ""
+}
+form.addEventListener("submit", validation);
 
+function validation (evt) {
+    evt.preventDefault();
+    
+    const{
+elements:{email,password}
+} = evt.currentTarget;
+if(!email.value || !password.value){
+    return alert ("Всі поля повинні бути заповнені!");
+}
+data.email = email.value;1
+data.password = password.value
+evt.currentTarget.reset();
+return console.log(data)
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,17 @@
-const button = document.querySelector('#btn');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const changeBtn = document.querySelector('.change-color');
+
+const span = document.querySelector('.color')
 
 const body = document.querySelector('body');
 
-const value = document.querySelector('span.color');
 
-const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']; 
-
-button.addEventListener('click', getValue);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+changeBtn.addEventListener("click", color)
+function color(evt) {
+  const color = getRandomHexColor();
+  span.textContent = color
+  body.style.backgroundColor = color
 }
