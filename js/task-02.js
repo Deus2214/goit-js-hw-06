d320f60fd06a2e5d78065a26bf62673b3c8b8943
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-12 23:12:08 +0300

    Comit

--- js/task-02.js
+++ js/task-02.js
@@ -1,46 +1,14 @@
 const ingredients = ["Potatoes","Mushrooms","Garlic","Tomatos"," Herbs,Condiments"];
 
+const list = document.querySelector ('#ingredients');
 
+const markup = ingredients.map (ingredient => {
+  const li = document.createElement ('li');
+  li.textContent = ingredient;
+  return li
+})
 
-const textElement = document.getElementById('ingredients');
-
-
-const newElement = document.createElement('li');
-newElement.innerHTML = 'Potatoes'
-console.log(newElement);
-textElement.append(newElement);
-newElement.classList.add("item");
-
-const secondChild = document.createElement('li');
-secondChild.innerHTML = 'Mushrooms'
-console.log(secondChild);
-textElement.append(secondChild);
-secondChild.classList.add("item");
-
-const thirdChild = document.createElement('li');
-thirdChild.innerHTML = ' Garlic'
-console.log(thirdChild);
-textElement.append(thirdChild);
-thirdChild.classList.add("item");
-
-const nextSibling = document.createElement('li');
-nextSibling .innerHTML = ' Tomatos'
-console.log(nextSibling );
-textElement.append(nextSibling );
-nextSibling .classList.add("item");
-
-const fifthChild = document.createElement('li');
-fifthChild  .innerHTML = ' Herbs'
-console.log(fifthChild );
-textElement.append(fifthChild  );
-fifthChild .classList.add("item");
-
-const lastChild = document.createElement('li');
-lastChild .innerHTML = ' Condiments'
-console.log(lastChild );
-textElement.append(lastChild );
-lastChild.classList.add("item");
-
+list.append(...markup)
 
 
 
@@ -62,6 +30,59 @@ lastChild.classList.add("item");
 // const itemsElement = makeMarkupCategories(ingredients);
 // listElements.append(...itemElements);
 // console.log(ingredients);
+
+
+
+
+
+
+
+
+// const textElement = document.getElementById('ingredients');
+
+
+// const newElement = document.createElement('li');
+// newElement.innerHTML = 'Potatoes'
+// console.log(newElement);
+// textElement.append(newElement);
+// newElement.classList.add("item");
+
+// const secondChild = document.createElement('li');
+// secondChild.innerHTML = 'Mushrooms'
+// console.log(secondChild);
+// textElement.append(secondChild);
+// secondChild.classList.add("item");
+
+// const thirdChild = document.createElement('li');
+// thirdChild.innerHTML = ' Garlic'
+// console.log(thirdChild);
+// textElement.append(thirdChild);
+// thirdChild.classList.add("item");
+
+// const nextSibling = document.createElement('li');
+// nextSibling .innerHTML = ' Tomatos'
+// console.log(nextSibling );
+// textElement.append(nextSibling );
+// nextSibling .classList.add("item");
+
+// const fifthChild = document.createElement('li');
+// fifthChild  .innerHTML = ' Herbs'
+// console.log(fifthChild );
+// textElement.append(fifthChild  );
+// fifthChild .classList.add("item");
+
+// const lastChild = document.createElement('li');
+// lastChild .innerHTML = ' Condiments'
+// console.log(lastChild );
+// textElement.append(lastChild );
+// lastChild.classList.add("item");
+
+
+
+
+
+
+
 
 
 
--- js/task-03.js
+++ js/task-03.js
@@ -12,24 +12,37 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
-const textElement = document.querySelector(".gallery");
-console.log(textElement)
-textElement.insertAdjacentHTML(
-  'afterbegin', '<li><img  src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="White and Black Long Fur Cat"></li>'
-);
+const textElement = document.querySelector('.gallery');
+const markup = images.map(({ url, alt }) => `<li>
+<img src="${url}" 
+alt="${alt}" 
+width="300" height="200">
+</li>`).join("")
+textElement.insertAdjacentHTML("beforeend", markup);
 
 
-textElement.insertAdjacentHTML(
-  'beforeend', '<li><img  src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Orange and White Koi Fish Near Yellow Koi Fish"></li>'
-);
 
-textElement.insertAdjacentHTML(
-  'beforeend', '<li><img  src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Group of Horses Running"></li>'
-);
-textElement.style.display = "flex";
-textElement.style.gap = "20px";
-textElement.style.width = "300px";
-textElement.style.height = "300px";
+
+
+
+// const textElement = document.querySelector(".gallery");
+// console.log(textElement)
+// textElement.insertAdjacentHTML(
+//   'afterbegin', '<li><img  src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="White and Black Long Fur Cat"></li>'
+// );
+
+
+// textElement.insertAdjacentHTML(
+//   'beforeend', '<li><img  src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Orange and White Koi Fish Near Yellow Koi Fish"></li>'
+// );
+
+// textElement.insertAdjacentHTML(
+//   'beforeend', '<li><img  src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Group of Horses Running"></li>'
+// );
+// textElement.style.display = "flex";
+// textElement.style.gap = "20px";
+// textElement.style.width = "300px";
+// textElement.style.height = "300px";
 
 
 // const elements = images.map(({ url, alt }) => {
