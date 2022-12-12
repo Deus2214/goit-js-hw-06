31ecafe27cc028b75c17ae9eea7487e7daab990a
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-12 16:28:59 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -17,9 +17,23 @@ console.log(textElement)
 textElement.insertAdjacentHTML(
   'afterbegin', '<li><img  src = "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="White and Black Long Fur Cat"></li>'
 );
+
+
 textElement.insertAdjacentHTML(
   'beforeend', '<li><img  src = "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Orange and White Koi Fish Near Yellow Koi Fish"></li>'
 );
+
 textElement.insertAdjacentHTML(
   'beforeend', '<li><img  src = "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt ="Group of Horses Running"></li>'
 );
+textElement.style.display = "flex";
+textElement.style.gap = "20px";
+textElement.style.width = "300px";
+textElement.style.height = "300px";
+
+
+// const elements = images.map(({ url, alt }) => {
+//   const markup = ` <li><img class="image" src= ${url} alt= ${alt} width=400 /></li> `;
+//   return markup;
+// })
+// .join("");
